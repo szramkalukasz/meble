0a6935a9ba24e234cabc54f17c9b254e05975ffd
Lukasz Szramka <lukasz_szramka@wp.pl>
2022-03-07 22:30:36 +0100

    I made a photo changer banner and added styles in scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,42 @@
 import '../scss/main.scss';
 
 
-console.log('HELLO 🚀')
+const slideList = [{
+        img: "../assets/img/kitchen-ga4c1e6c4b_1280.jpg",
+        text: 'Kuchnie na wymiar'
+    },
+    {
+        img: "../assets/img/wardrobe-g711ff2ee2_1280.jpg",
+        text: 'Zabudowy na wymiar'
+    },
+    {
+        img: "../assets/img/bathroom-g09b3fa509_1280.jpg",
+        text: 'Łazienki na wymiar'
+    }
+];
+
+const image = document.querySelector('img.header__slider--js');
+const h1 = document.querySelector('h1.header__title--js');
+const dots = [...document.querySelectorAll('.dots span')]
+// Interfejs
+const time = 3000;
+let active = 0;
+
+//Implementacje
+
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+const changeSlide = () => {
+    active++;
+    if (active === slideList.length) {
+        active = 0;
+    }
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    changeDot()
+}
+setInterval(changeSlide, time)
