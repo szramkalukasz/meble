1779e6c063fb9b1e219e0f6b6db4f2f09d5d2919
Lukasz Szramka <lukasz_szramka@wp.pl>
2022-03-10 22:12:19 +0100

    I added navigation to the header section

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,17 @@
 import '../scss/main.scss';
 
 
+
 const slideList = [{
-        img: "../assets/img/kitchen-ga4c1e6c4b_1280.jpg",
+        img: "src/assets/img/kitchen-ga4c1e6c4b_1280.jpg",
         text: 'Kuchnie na wymiar'
     },
     {
-        img: "../assets/img/wardrobe-g711ff2ee2_1280.jpg",
+        img: "src/assets/img/wardrobe-g711ff2ee2_1280.jpg",
         text: 'Zabudowy na wymiar'
     },
     {
-        img: "../assets/img/bathroom-g09b3fa509_1280.jpg",
+        img: "src/assets/img/bathroom-g09b3fa509_1280.jpg",
         text: 'Łazienki na wymiar'
     }
 ];
@@ -19,7 +20,7 @@ const image = document.querySelector('img.header__slider--js');
 const h1 = document.querySelector('h1.header__title--js');
 const dots = [...document.querySelectorAll('.dots span')]
 // Interfejs
-const time = 3000;
+const time = 4000;
 let active = 0;
 
 //Implementacje
